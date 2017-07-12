6668ae4d0135d7619eaeea6d84bf0f0da5b42bdd
Jack Molosh <jackmolosh@gmail.com>
2017-07-12 16:14:14 +0300

    Some more corretings

--- src/response_sender.rb
+++ src/response_sender.rb
@@ -1,30 +1,30 @@
 class ResponseSender
-  attr_reader :response, :message, :bot
+  attr_reader :response, :chat_id, :bot
 
   def initialize(handler, *args)
     @bot = handler.bot
-    @message = handler.message
+    @chat_id = handler.message.chat.id
     @response = args[0]
 
     send_response
   end
 
   def send_response
-    response.fetch(:type).equal?('document') ? send_document : send_message
+    response[:type].equal?('document') ? send_document : send_message
   end
 
   private
 
   def send_message
     bot.api.send_message(
-      chat_id: message.chat.id,
+      chat_id: chat_id,
       text: response.fetch(:content)
     )
   end
 
   def send_document
     bot.api.send_document(
-      chat_id: message.chat.id,
+      chat_id: chat_id,
       document: Faraday::UploadIO.new(response.fetch(:content), 'text/plain')
     )
   end
